4c01b3f87f097c90f3cbe37bd9b5f5ad35d8a113
syh213 <syh21133@gmail.com>
2024-09-04 16:35:26 +0900

    필수과제 완료

---
+++ src/com/example/calculator/Calculator.java
@@ -0,0 +1,90 @@
+package com.example.calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+
+    private int firstNumber;
+    private int secondNumber;
+    static int count = 0;
+    private static final List<Integer> calResult = new ArrayList<Integer>();
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String calResult() {
+        return calResult.toString();
+    }
+
+    public int getFirstNumber() {
+        return firstNumber;
+    }
+
+    public void setFirstNumber(int firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public int getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setSecondNumber(int secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public String Cal(int a, int b, String c) {
+        String answer = "";
+
+        switch (c) {
+            case "+" -> {
+                answer = a + " " + c + " " + b + " = " + (a + b);
+                Calculator.addResult(a+b);
+            }
+            case "-" -> {
+                answer = a + " " + c + " " + b + " = " + (a - b);
+                Calculator.addResult(a-b);
+            }
+            case "*" -> {
+                answer = a + " " + c + " " + b + " = " + (a * b);
+                Calculator.addResult(a*b);
+            }
+            case "/" -> {
+
+                try {
+
+                    answer = a + " " + c + " " + b + " = " + (a / b);
+                    Calculator.addResult(a/b);
+                    // 0으로 나눴을때 예외처리
+                } catch (ArithmeticException e) {
+                    System.out.println("0으로 나눌수 없습니다.");
+
+                }
+            }
+        }
+        return answer;
+
+
+
+    }
+
+    public static void addResult(int cal) {
+        if(count<3){
+            calResult.add(cal);
+            count++;
+        }else{
+            calResult.remove(0);
+            calResult.add(cal);
+        }
+
+
+    }
+    public List<Integer> printResult(){
+
+        return calResult;
+    }
+
+
+
+}
---
+++ src/com/example/calculator/Main.java
@@ -0,0 +1,109 @@
+package com.example.calculator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        // 필수 과제 쿨래스 사용 x
+//        Scanner scanner = new Scanner(System.in);
+//        Boolean exit = true;
+//
+//
+//        while(exit) {
+//            System.out.print("첫 번째 숫자 입력 : ");
+//            int first = scanner.nextInt();
+//            System.out.print("사칙연산 기호 입력 : ");
+//            String operator = scanner.next();
+//            System.out.print("두 번째 숫자 입력 : ");
+//            int second = scanner.nextInt();
+//
+//            if (first >= 0 && second >= 0) {
+//
+//
+//                switch (operator) {
+//                    // 덧셈
+//                    case "+":
+//                        System.out.println(first + " " + operator + " " + second + " = " + (first + second));
+//                        break;
+//                    // 뺄셈
+//                    case "-":
+//                        System.out.println(first + " " + operator + " " + second + " = " + (first - second));
+//                        break;
+//                    // 곱셈
+//                    case "*":
+//                        System.out.println(first + " " + operator + " " + second + " = " + first * second);
+//                        break;
+//                    // 나눗셈
+//                    case "/":
+//                        try {
+//
+//                            System.out.println(first + " " + operator + " " + second + " = " + first / second);
+//                            break;
+//
+//                            // 0으로 나눴을때 예외처리
+//                        } catch (ArithmeticException e) {
+//                            System.out.println("0으로 나눌수 없습니다.");
+//
+//                        }
+//                        break;
+//                    default:
+//                        System.out.println("잘못 입력 하셨습니다.");
+//                }
+//                System.out.println("계산기를 종료하시겠습니까?(exit입력 시 종료)");
+//                String answer = scanner.next();
+//
+//                if (answer.equals("exit")) {
+//                    System.out.println("계산기를 종료합니다.");
+//                    exit = false;
+//                } else {
+//                    continue;
+//                }
+//            }else{
+//                System.out.println("잘못 입력 하셨습니다.");
+//
+//            }
+//
+//
+//        }
+
+
+        Calculator calc = new Calculator();
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = true;
+
+
+        while (exit) {
+            System.out.print("첫 번째 숫자 입력 : ");
+            int first = scanner.nextInt();
+            System.out.print("사칙연산 기호 입력 : ");
+            String operator = scanner.next();
+            System.out.print("두 번째 숫자 입력 : ");
+            int second = scanner.nextInt();
+
+            if (first >= 0 && second >= 0) {
+
+                System.out.println(calc.Cal(first, second, operator));
+
+                System.out.println("저장된 계산값 : "+Calculator.calResult());
+                System.out.println("계산기를 종료하시겠습니까?(exit입력 시 종료)");
+                String answer = scanner.next();
+
+                if (answer.equals("exit")) {
+                    System.out.println("계산기를 종료합니다.");
+                    exit = false;
+                } else {
+                    continue;
+                }
+            } else {
+                System.out.println("잘못 입력 하셨습니다.");
+
+            }
+
+
+        }
+
+
+    }
+}
